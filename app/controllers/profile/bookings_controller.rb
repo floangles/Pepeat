41e47462673b067f18e4040ef9ad15c865ff7cc7
Jonathan Maurin <jonathan@deux.io>
2015-06-30 18:37:48 +0200

    home css

---
+++ app/controllers/profile/bookings_controller.rb
@@ -0,0 +1,13 @@
+module Profile
+  class BookingsController < ApplicationController
+    before_action :authenticate_user!
+    def index
+      @bookings = current_user.bookings
+    end
+    def show
+      @booking = Booking.find(params[:id])
+    end
+
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :recipes
 
   resource :profile, only: [:show, :edit, :update]
+
+  namespace :profile do
+    resources :bookings, only: [:index, :show]
+  end
 end
