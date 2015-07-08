91d83fba7c4fe6f7e8e38b31ba873ee0b7295564
florent angles <florent.angles@hei.fr>
2015-07-08 17:01:32 +0200

    first commit

--- app/controllers/profile/bookings_controller.rb
+++ app/controllers/profile/bookings_controller.rb
@@ -3,9 +3,13 @@ module Profile
     before_action :authenticate_user!
      #si je suis un utilisateur lambda
 
+
+    def index
+      @bookings = current_user.bookings.all
+    end
+
+
      def new
-
-
       @offer = Offer.find(params[:booking][:offer_id])
       @offer = recipe.offers
       @booking = Booking.new
@@ -17,7 +21,7 @@ module Profile
       @booking.user = current_user
       @booking.save
       if @booking.save
-        redirect_to profile_offers_path
+        redirect_to profile_bookings_path
       else
         render :new
       end
